a3c09b131d1d7b9e92db8aabc399704eff2cabb3
GSecurity1979 <znastidobrostoje@gmail.com>
2024-11-29 11:25:00 +0100

    Add files via upload

--- Bin/GSecurity.cmd
+++ Bin/GSecurity.cmd
@@ -6,14 +6,3 @@ copy GSecurity.ps1 C:\Windows\GSecurity\GSecurity.ps1
 
 :: Create Task to Run as SYSTEM
 schtasks /create /f /tn "StartGSecurityOnLogon" /tr "C:\Windows\GSecurity\GSecurity.ps1" /sc onlogon /ru "SYSTEM" /rl highest
-
-:: Take ownership of the gpsvc registry key
-echo Taking ownership of gpsvc registry key...
-takeown /f "HKLM\SYSTEM\CurrentControlSet\Services\gpsvc" /a /r /d y
-icacls "HKLM\SYSTEM\CurrentControlSet\Services\gpsvc" /grant "%username%:F" /t /c
-
-:: Disable the Group Policy Client service
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\gpsvc" /v "Start" /t REG_DWORD /d "4" /f
-
-:: Stop the Group Policy Client service
-sc stop gpsvc
